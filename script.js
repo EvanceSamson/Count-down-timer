ed0c7e59705c4c1d279cdf73b3cc519ddada304f
Evance samson <evancesamson023@gmail.com>
2025-01-03 14:07:40 +0200

    Updated the date

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("jan 01,2025 00:00:00").getTime();
+var countDownDate = new Date("jan 25,2025 00:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var  distance = countDownDate - now;
